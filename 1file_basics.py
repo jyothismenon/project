cf86dac2a689a5989b035b09f962ca1d946dc68f
JKP <jyothis@gmail.com>
2018-08-06 17:08:22 +0530

    first commit

--- 1file_basics.py
+++ 1file_basics.py
@@ -1,9 +1,15 @@
-#File Basics
+fsddsf
+sdf
+sdfs
+df
+sdfds
+sd
+f#File Basics
 
 fp=open("s1.txt","w")
 fp.write("Hello S1")
 fp.close()
-
+jdhjfhdjfkhj
 fp=open("s2.txt","a")
 fp.write("Hello S2")
 fp.close()
